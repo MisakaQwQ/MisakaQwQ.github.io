f3be903897cb699ff9c8d11fac634f0affc1c8bc
MisakaQwQ <charqyq@gmail.com>
2021-04-21 19:56:05 +0800

    修改问题

--- data/content/pipe/pipe2.js
+++ data/content/pipe/pipe2.js
@@ -1,6 +1,6 @@
-correct_answer = [parseInt("010001", 2), parseInt("100001", 2), parseInt("000011", 2),
-                parseInt("000100", 2), parseInt("010000", 2), parseInt("100100", 2),
-                parseInt("000110", 2), parseInt("000001", 2), parseInt("001000", 2)]
+correct_answer = [parseInt("1100000010", 2), parseInt("1000010001", 2), parseInt("1011000000", 2),
+                parseInt("0000100100", 2), parseInt("0100000010", 2), parseInt("0000110001", 2),
+                parseInt("0011100000", 2), parseInt("1000000100", 2), parseInt("0000001100", 2)]
 now_status = 0
 score = 0
 score_status = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -19,7 +19,7 @@ function lock_form(){
     for(var i=0;i<check.length;i+=1){
         check[i].disabled = true;
     }
-    var btn = document.getElementsByTagName('button')[0];
+    var btn = document.getElementById('animation_submit');
     btn.disabled = true;
 }
 
@@ -28,8 +28,47 @@ function release_form(){
     for(var i=0;i<check.length;i+=1){
         check[i].disabled = false;
     }
-    var btn = document.getElementsByTagName('button')[0];
+    var btn = document.getElementById('animation_submit');
     btn.disabled = false;
+}
+
+function judge_choice(){
+    var choice1a = document.getElementsByName('choice1a')[0];
+    var choice1b = document.getElementsByName('choice1b')[0];
+    var choice2a = document.getElementsByName('choice2a')[0];
+    var choice2b = document.getElementsByName('choice2b')[0];
+    lock_form();
+    if(choice1a.checked && choice1b.checked) {
+        score+=5;
+    }
+    if(choice2a.checked && choice2b.checked) {
+        score+=5;
+    }
+    if(!choice1a.checked) {
+        choice1a.checked = true;
+        document.getElementById('choice_1a').style.color = '#ff0000';
+    }
+    if(!choice1b.checked) {
+        choice1b.checked = true;
+        document.getElementById('choice_1b').style.color = '#ff0000';
+    }
+    if(!choice2a.checked) {
+        choice2a.checked = true;
+        document.getElementById('choice_2a').style.color = '#ff0000';
+    }
+    if(!choice2b.checked) {
+        choice2b.checked = true;
+        document.getElementById('choice_2b').style.color = '#ff0000';
+    }
+    btn = document.getElementById('choose_submit');
+    btn.innerHTML = "进入管路仿真";
+    btn.onclick = enter_simulate;
+}
+
+function enter_simulate(){
+    release_form();
+    document.getElementsByClassName('prev_question')[0].style.display = "none";
+    document.getElementsByClassName('main_container')[0].style.display = "block";
 }
 
 function judge(){
@@ -40,27 +79,30 @@ function judge(){
     var check = document.getElementsByTagName('input');
     for(var i=0;i<check.length;i+=1){
         if(check[i].checked){
-            now += 1<<i;
+            now += parseInt(check[i].value);
         }
     }
     if(correct_answer[now_status] == now)
     {
         lock_form();
         if(score_status[now_status] == 0) {
-            score += 1
+            score += 10
         }
         now_status += 1
         console.log("Correct");
-        document.getElementById('title').innerHTML = title[now_status - 1];
+        document.getElementById('title').innerHTML = '当前任务：' + title[now_status - 1];
         var video = document.getElementsByTagName("video")[0];
         video.src = now_status + '.mp4'
+        video.playbackRate=5;
         video.play();
         video.addEventListener('ended', function () {
             if(now_status==9){
-                document.getElementById('notify').innerHTML = '总得分' + score + '/9';
+                document.getElementById('notify').innerHTML = '得分' + score;
             }
             else{
-                document.getElementById('notify').innerHTML = '下一个任务' + title[now_status];
+                document.getElementById('title').innerHTML = '当前任务：' + title[now_status];
+                document.getElementById('notify').innerHTML = '';
+                // document.getElementById('notify').innerHTML = '下一个任务' + title[now_status];
                 clear_choice();
                 release_form();
             }

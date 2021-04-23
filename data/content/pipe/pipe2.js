fa2e972e4688a16aae5bc7ea3e863ce754716a40
MisakaQwQ <charqyq@gmail.com>
2021-04-23 23:22:46 +0800

    增加解析，内容修改

--- data/content/pipe/pipe2.js
+++ data/content/pipe/pipe2.js
@@ -6,6 +6,15 @@ score = 0
 score_status = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 title = ["稀释液充满小孔管及其管道", "清洗液冲洗管道", "稀释液充满小孔管及其管道", "样品液流通路径", "定量管恢复大气压", "清洗过程",
     "清洗管道，排除气泡", "除堵过程", "血红蛋白测定"]
+tip = ["此过程为排空定量装置内液体。A、X阀门打开，上次操作过程中的定量管内液体被负压抽吸进入废液瓶，否则残留定量管内的液体影响体积的定量。",
+        "此过程为清洗液冲洗管道。为血细胞计数做准备，先把管道用清洗液清洗一下。其路径为清洗液→Z阀→小孔管→A阀→废液瓶。",
+        "此过程为稀释液充满小孔管及其管道。其路径为稀释液→B阀→小孔管→A阀→废液瓶。",
+        "此过程为样品液流通路径。其路径为样品液→小孔→小孔管→定量管→C阀→负压缓冲瓶→废液瓶。",
+        "此过程为定量管恢复大气压。样品液到达P3时，门电路开始计数，到达P1时，停止计数。随后C阀关闭，X阀打开0.3s，定量管恢复大气压状态。",
+        "此过程为清洗过程。按下“清洗”键时，Z、A、C阀同时打开5s，清洗液路径为：<img src=\"8.png\" alt=\"\">",
+        "按下“供水”时，B、C阀打开5s。稀释液的路径为：稀释液→缓冲瓶→B阀→小孔管→定量管→C阀→负压缓冲瓶→废液瓶。此过程为清洗管道，排除气泡。",
+        "此过欻为除堵过程。小孔管堵塞时，按下“排堵”键（即“清除”键），并移去样品杯，空气从小孔管吸入，去除堵塞，此时A阀开通。",
+        "此过程为血红蛋白测定过程。D阀打开，根据Lambert-Beer定律进行比色分析。"]
 
 function clear_choice(){
     var check = document.getElementsByTagName('input');
@@ -60,6 +69,7 @@ function judge_choice(){
         choice2b.checked = true;
         document.getElementById('choice_2b').style.color = '#ff0000';
     }
+    document.getElementById("answer_1a").style.display = 'block';
     btn = document.getElementById('choose_submit');
     btn.innerHTML = "进入管路仿真";
     btn.onclick = enter_simulate;
@@ -127,7 +137,7 @@ function judge(){
                 btn.onclick=show_result;
             }
         }, false);
-        document.getElementById('notify').innerHTML = '选择正确<br>解析：<br>...';
+        document.getElementById('notify').innerHTML = '选择正确<br>解析：' + tip[now_status - 1];
     }
     else{
         score_status[now_status] = -1
